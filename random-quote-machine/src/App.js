e31d3f6eb704684302d4e7e996c69f6c5fde95a8
leebissessar5 <leebissessar@gmail.com>
2023-06-18 21:31:04 -0400

    move Footer to app

--- random-quote-machine/src/App.js
+++ random-quote-machine/src/App.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const baseUrl = 'https://api.quotable.io/random'
+
+const Footer = () => (
+    <div className="footer">
+        Created by Lee Bissessar
+        <p>Built using React, Bootstrap, and Sass</p>
+    </div>
+)
 
 const App = () => {
     const [quote, setQuote] = useState(null)
@@ -26,22 +35,36 @@ const App = () => {
     return (
         <div id="quote-box">
             {quote && (
-                <>
+                <div className="quote-container">
                     <div id="text">{quote.content}</div>
                     <div id="author">- {quote.author}</div>
-                    <button id="new-quote" onClick={handleNewQuote}>
-                        New Quote
-                    </button>
-                    <a
-                        id="tweet-quote"
-                        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-                            `${quote.content} - ${quote.author}`
-                        )}`}
-                    >
-                        Tweet Quote
-                    </a>
-                </>
+                    <div className="d-flex justify-content-between">
+                        <button
+                            id="new-quote"
+                            className="btn btn-primary"
+                            onClick={handleNewQuote}
+                        >
+                            New Quote
+                        </button>
+                        <a
+                            id="tweet-quote"
+                            className="btn btn-primary"
+                            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                                `${quote.content} - ${quote.author}`
+                            )}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Tweet Quote
+                            <FontAwesomeIcon
+                                className="twitter-icon"
+                                icon={faTwitter}
+                            />
+                        </a>
+                    </div>
+                </div>
             )}
+            <Footer />
         </div>
     )
 }

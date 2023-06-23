ae51813f649ad7f4abe5bc16e764747e2623c803
leebissessar5 <leebissessar@gmail.com>
2023-06-22 22:01:41 -0400

    upload initial attempt

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col, Form, FormControl, Card } from 'react-bootstrap'
+import Markdown from 'marked-react'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [markdown, setMarkdown] = useState('')
+
+    useEffect(() => {
+        // Set default markdown content
+        const defaultMarkdown = `
+      # Heading 1
+      ## Heading 2
+      [Link](https://www.example.com)
+      \`inline code\`
+      \`\`\`
+      // Code block
+      function greet() {
+        console.log('Hello!');
+      }
+      \`\`\`
+      - List item
+      > Blockquote
+      ![Image](https://via.placeholder.com/150)
+      **Bold text**
+    `
+        setMarkdown(defaultMarkdown)
+    }, [])
+
+    const handleInputChange = (event) => {
+        setMarkdown(event.target.value)
+    }
+
+    console.log(markdown)
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1 className="text-center">Markdown Previewer</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={6}>
+                    <Card>
+                        <Card.Body>
+                            <Form>
+                                <FormControl
+                                    as="textarea"
+                                    id="editor"
+                                    value={markdown}
+                                    onChange={handleInputChange}
+                                />
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={6}>
+                    <Card>
+                        <Card.Body>
+                            <div id="preview">
+                                <Markdown value={markdown} />
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
-export default App;
+export default App

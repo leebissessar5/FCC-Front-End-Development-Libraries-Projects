7f7a6138c0ddf0c8586405550c297e14735025f4
leebissessar5 <leebissessar@gmail.com>
2023-06-23 15:53:08 -0400

    add some more styling

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -42,7 +42,7 @@ const App = () => {
     return (
         <Container>
             <Row>
-                <Col md={6} lg={5}>
+                <Col md={6} lg={4}>
                     <Card>
                         <Card.Body>
                             <Form>
@@ -57,7 +57,7 @@ const App = () => {
                         </Card.Body>
                     </Card>
                 </Col>
-                <Col md={6} lg={7}>
+                <Col md={6} lg={8}>
                     <Card>
                         <Card.Body>
                             <Form.Label htmlFor="preview">Previewer</Form.Label>
